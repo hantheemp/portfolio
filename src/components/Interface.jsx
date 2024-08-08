1112ec3eda1762fea806b87e6eeb51c3b56668ed
hantheemp <temelmuratkagan@gmail.com>
2024-08-08 17:13:31 +0300

    polish

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -39,7 +39,7 @@ const Section = (props) => {
 
 export const Interface = () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col overflow-auto">
       <Section>
         <About></About>
       </Section>
